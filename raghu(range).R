745350673bf2be462ad10b275c28f55c1c4678eb
Raghu1426 <113841807+Raghu1426@users.noreply.github.com>
2023-05-09 19:43:17 +0530

    Add files via upload

---
+++ raghu(range).R
@@ -0,0 +1,8 @@
+# Create a dataset
+data <- c(2, 5, 7, 8, 10, 11, 13, 15, 18, 20)
+
+# Calculate the range
+data_range <- range(data)
+
+# Print the range
+cat("Range: ", data_range[2] - data_range[1])
