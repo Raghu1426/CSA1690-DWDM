85e90dd23ff78192232fcee12d175804140843f8
Raghu1426 <113841807+Raghu1426@users.noreply.github.com>
2023-05-09 18:55:05 +0530

    Delete raghu(piechart).R

--- raghu(piechart).R
+++
@@ -1,9 +0,0 @@
-#Create data for the graph.
-x<-c(21,62,10,53)
-labels<-c("Paris","London","New York","Washington")
-#Give the chart file.
-png(file="city.png")
-#Plot the chart.
-pie(x,labels)
-#save the file.
-dev.off()
